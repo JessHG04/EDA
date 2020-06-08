c409474f99146f20be9022ca468392304d73e79c
JessHG04 <jessicahg04@gmail.com>
2020-06-08 21:47:21 +0200

    Arreglo p2

--- Práctica 2/Crucero.cc
+++ Práctica 2/Crucero.cc
@@ -5,28 +5,12 @@
 #include "Provincia.h"
 
 int main(int argc, char *argv[]){
-    bool aux = true;
-    int x = 10000000;
-    int y = 0;
+    Coleccion colec = Coleccion();
 
-    std::cout << "Te Puru Saint Andrews 13" << std::endl;
-    std::cout << "Saint Andrews Motatau 13" << std::endl;
-    while(aux && y<=x){
-        y++;
-    }
-    y = 0;
-    std::cout << "Motatau Putaruru 10" << std::endl;
-    std::cout << "Putaruru Parua Bay 4" << std::endl;
-    while(aux && y<=x){
-        y++;
-    }
-    y = 0;
-    std::cout << "Parua Bay Te Puru 11" << std::endl;
-    std::cout << "total=51" << std::endl;
-    while(aux && y<=x){
-        y++;
-    }
-    y = 0;
-    std::cout << "011021077707034334311101024465444444324460777754555" << std::endl;    
+    if(argc == 2){
+        colec.lectura(argv[1]);
+
+        
+    }   
 
 }
