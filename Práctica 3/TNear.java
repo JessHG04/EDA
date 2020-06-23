33b15eb858c3f1be476f0508f2991921e3d2ec72
JessHG04 <jessicahg04@gmail.com>
2020-06-23 15:17:53 +0200

    P3

--- Práctica 3/TNear.java
+++ Práctica 3/TNear.java
@@ -23,11 +23,26 @@ public class TNear {
         return devolver;
     }
 
+    public int distanciaManhattan(int x1, int x2, int y1, int y2){
+        int distancia = 0;
+
+        distancia = Math.abs(x1 - y1) + Math.abs(x2 - y2);
+
+        return distancia;
+    }
+
     public void insertaLocalidad2(Localidad2 w){
-        TreeSet<Localidad2> insertar = null;
+        TreeSet<Localidad2> insertar = new TreeSet<>();
+        boolean vacio = false;
         
-        if(w!= null && ti.containsKey(w) == false){
-            
+        if(w!= null && !ti.containsKey(w) ){
+            //Anyadimos la localidad
+            vacio = esVacio();
+            ti.put(w, insertar);
+            //Comprobamos si hay que incluirla en el TreeMap
+            if(!vacio){
+
+            }
         }
     }
 
@@ -45,7 +60,25 @@ public class TNear {
 
     public TreeSet<Localidad2> getLocalidades(String s){
         TreeSet<Localidad2> devolver = null;
-
+        /*Iterator<Localidad2> it = ti.iterator();
+        boolean encontrado = false;
+        Localidad2 loca = null;
+        if(s!= null)){
+            while(it.hasNext() && !encontrado){
+                loca = it.next();
+                if(loca.getNombre().equalsIgnoreCase(s)){
+                    encontrado = true;
+                }
+                devolver = ti.get(it);
+            }
+        }*/
+        if(s!= null){
+            Localidad loc = new Localidad(s);
+            Localidad2 locc = new Localidad2(loc);
+            if(ti.containsKey(locc)){
+                devolver = ti.get(locc);
+            }
+        }
         return devolver;
     }
 
@@ -61,7 +94,18 @@ public class TNear {
 
     public String toString(){
         String devolver = "";
+        Set<Localidad2> principal = ti.keySet();
+        TreeSet<Localidad2> cercanas;
+        boolean primero = true;
 
+        for(int x = 0; x < principal.size(); x++){
+            Object[] parray = principal.toArray();
+            String l = (Localidad2)parray[x].getNombre();
+            primero = true;
+            cercanas = ti.get((Localidad2)parray[x]);
+            devolver += l + " * ";
+
+        }
         return devolver;
     }
 
